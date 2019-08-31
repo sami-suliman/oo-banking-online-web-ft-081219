959624e6e08d885d2b0a0d5a55c25323ac47bf32
sami suliman <sami.bco@gmail.com>
2019-08-31 23:18:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,11 @@
 class BankAccount
-  attr_accessor :balance
+  attr_accessor :balance, :status
   attr_reader :name 
 
   def initialize(account_name)
     @name = account_name
     @balance = 1000
+    @status = "open"
   end 
   
 end
